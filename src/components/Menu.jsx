58ab1a2e0d14dfe664747dc8f140aff3b335efc3
Zéias <oseias.augusto@aluno.ifsp.edu.br>
2025-09-19 20:31:02 -0300

    Update Menu.jsx

--- src/components/Menu.jsx
+++ src/components/Menu.jsx
@@ -1,10 +1,10 @@
-export default function Menu({ setModo, titulo, reset, menu }){
+export default function Menu({ setModo, titulo, reset2, menu }){
     return (
         <>
             <div className={menu}> 
                 <h2 className={titulo}>Modo de Jogo</h2> 
-                <button className={reset} onClick={() => setModo('normal')}>Normal</button> 
-                <button className={reset} onClick={() => setModo('limitado')}>Limitado</button> 
+                <button className={reset2} onClick={() => setModo('normal')}>Normal</button> 
+                <button className={reset2} onClick={() => setModo('limitado')}>Limitado</button> 
             </div>
         
         </>
